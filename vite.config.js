6d52e4d16239e2f41bd8626d57f45ef26b0f4452
Xaliphostes <fmaerten@gmail.com>
2025-11-07 09:32:41 +0100

    last devs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
-    base: './src',
+    base: '/rupture-env',
     build: {
         outDir: 'dist',
         sourcemap: true,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: './src',
+    base: '/rupture-env',
     build: {
         outDir: 'dist',
         sourcemap: true,
